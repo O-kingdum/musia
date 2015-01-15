f0aadb7887cfb1fba8ea293c0b9498650e8e65ee
watanabe.yuta <watanabe.yuta@172.29.49.45>
2015-01-15 15:00:19 +0900

    add class checklist

---
+++ musia/src/jp/co/musia/okingdum/Check_List.java
@@ -0,0 +1,34 @@
+package jp.co.musia.okingdum;
+
+public class Check_List {
+
+	private String list_id;
+	private String check_list;
+	
+	/**
+	 * 
+	 * @param list_id : チェックリストID
+	 * @param check_list : チェックリスト項目
+	 *
+	 */
+	public Check_List(String list_id, String check_list) {
+		this.list_id = list_id;
+		this.check_list = check_list;
+	}
+
+	public String getList_id() {
+		return list_id;
+	}
+
+	public void setList_id(String list_id) {
+		this.list_id = list_id;
+	}
+
+	public String getCheck_list() {
+		return check_list;
+	}
+
+	public void setCheck_list(String check_list) {
+		this.check_list = check_list;
+	}
+}
