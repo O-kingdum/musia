557dc25b4e7313efbf907ea1b32b4aa23db196f2
watanabe.yuta <watanabe.yuta@ths30182>
2015-01-25 00:08:57 +0900

    komi

--- musia/src/jp/co/musia/okingdum/Contest_Music.java
+++ musia/src/jp/co/musia/okingdum/Contest_Music.java
@@ -4,4 +4,66 @@ public class Contest_Music {
 
 	private String contest_id;
 	private String product_id;
+	private String contest_admin_id;
+	private int contest_examination;
+	private String record_date;
+	
+	/**
+	 * 
+	 * @param contest_id : コンテストID
+	 * @param product_id : 商品ID
+	 * @param contest_admin_id : コンテスト登録管理者ID
+	 * @param contest_examination : コンテスト審査状況
+	 * @param record_date : 登録日付
+	 */
+	public Contest_Music(String contest_id, String product_id,
+			String contest_admin_id, int contest_examination,
+			String record_date) {
+		super();
+		this.contest_id = contest_id;
+		this.product_id = product_id;
+		this.contest_admin_id = contest_admin_id;
+		this.contest_examination = contest_examination;
+		this.record_date = record_date;
+	}
+
+	public String getContest_id() {
+		return contest_id;
+	}
+
+	public void setContest_id(String contest_id) {
+		this.contest_id = contest_id;
+	}
+
+	public String getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(String product_id) {
+		this.product_id = product_id;
+	}
+
+	public String getContest_admin_id() {
+		return contest_admin_id;
+	}
+
+	public void setContest_admin_id(String contest_admin_id) {
+		this.contest_admin_id = contest_admin_id;
+	}
+
+	public int getContest_examination() {
+		return contest_examination;
+	}
+
+	public void setContest_examination(int contest_examination) {
+		this.contest_examination = contest_examination;
+	}
+
+	public String getRecord_date() {
+		return record_date;
+	}
+
+	public void setRecord_date(String record_date) {
+		this.record_date = record_date;
+	}
 }
