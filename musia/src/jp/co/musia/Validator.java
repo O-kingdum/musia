8f4c2f8c3c2f189c3d3cb0236264535da56b25bd
Naventus <u.u.tau216@gmail.com>
2015-01-29 17:31:40 +0900

    aaa

--- musia/src/jp/co/musia/Validator.java
+++ musia/src/jp/co/musia/Validator.java
@@ -1,12 +1,15 @@
 package jp.co.musia;
 
 import org.apache.commons.lang3.StringUtils;
-
 import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+
 
 public class Validator {
 
+	// エラーメッセージリスト
 	private ArrayList<String> errmsg;
+	// バリデーション判定変数
 	private boolean valid;
 	
 	/**
@@ -23,9 +26,12 @@ public class Validator {
 	 * @param value String: 検索ワード
 	 * @return valid boolean: バリデーション判定
 	 */
-	public boolean getSearchMusic(String value) {
+	public boolean getSearchMusic(HttpServletRequest request) {
+		
 		this.errmsg = new ArrayList<String>();
 		this.valid = true;
+		// 検索ワード取得
+		String value = request.getParameter("search_word");
 		
 		if(StringUtils.isEmpty(value)) {
 			valid = false;
@@ -33,123 +39,130 @@ public class Validator {
 		} else {
 			if(value.length() > 20) {
 				valid = false;
-				errmsg.add("�ｽy�ｽﾈ厄ｿｽ�ｽ�ｽ20�ｽ�ｽ�ｽ�ｽ�ｽﾈ難ｿｽ�ｽﾅ難ｿｽﾍゑｿｽ�ｽﾄ会ｿｽ�ｽ�ｽ�ｽ�ｽ�ｽB");
+				errmsg.add("楽曲名は20文字以内で入力してください。");
 			}
 		}
 		return valid;
 	}
 	/**
-	 * getLoginValidation�ｽ�ｽ�ｽ\�ｽb�ｽh : �ｽ�ｽ�ｽO�ｽC�ｽ�ｽ�ｽ�ｽ�ｽﾌ難ｿｽﾍに対ゑｿｽ�ｽ�ｽo�ｽ�ｽ�ｽf�ｽ[�ｽV�ｽ�ｽ�ｽ�ｽ
+	 * getLoginValidationメソッド : ログイン処理に対するバリデーション
 	 * 
-	 * @param email String: �ｽ�ｽ�ｽ[�ｽ�ｽ�ｽA�ｽh�ｽ�ｽ�ｽX
-	 * @param password String:�ｽ@�ｽp�ｽX�ｽ�ｽ�ｽ[�ｽh
-	 * @return valid boolean:�ｽ@�ｽo�ｽ�ｽ�ｽf�ｽ[�ｽV�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ
+	 * @param email String: メールアドレス
+	 * @param password String: パスワード
+	 * @return valid boolean:　バリデーション判定
 	 */
-	public boolean getLoginValidation(String email, String password) {
-		// �ｽG�ｽ�ｽ�ｽ[�ｽ�ｽ�ｽb�ｽZ�ｽ[�ｽW�ｽ�ｽ�ｽX�ｽg�ｽ�ｽ�ｽ�ｽ
+	public boolean getLoginValidation(HttpServletRequest request) {
 		this.errmsg = new ArrayList<String>();
-		// �ｽo�ｽ�ｽ�ｽf�ｽ[�ｽV�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ闖会ｿｽ�ｽ
 		this.valid = true;
 		
+		// メールアドレス取得
+		String email = request.getParameter("email");
+		// パスワード取得
+		String password = request.getParameter("password");
+		
 		/* email Validation */
-		if(StringUtils.isEmpty(email)) {	// �ｽ�ｽﾜゑｿｽ�ｽ�ｽnull
+		if(StringUtils.isEmpty(email)) {	//　空またはnull
 			valid = false;
-			errmsg.add("�ｽ�ｽ�ｽ[�ｽ�ｽ�ｽA�ｽh�ｽ�ｽ�ｽX�ｽ�ｽ�ｽﾍゑｿｽ�ｽﾄゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽB");
+			errmsg.add("メールアドレスを入力してください。");
 		} else {
 			if(!email.matches("[0-9a-zA-Z]+")) {
 				valid = false;
-				errmsg.add("�ｽ�ｽ�ｽ[�ｽ�ｽ�ｽA�ｽh�ｽ�ｽ�ｽX�ｽﾍ費ｿｽ�ｽp�ｽp�ｽ�ｽ�ｽ�ｽ�ｽﾅ難ｿｽﾍゑｿｽ�ｽﾄゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽB");
+				errmsg.add("メールアドレスは半角英数字で入力してください。");
 			}			
 			if(email.length() > 50) {
 				valid = false;
-				errmsg.add("�ｽ�ｽ�ｽ[�ｽ�ｽ�ｽA�ｽh�ｽ�ｽ�ｽX�ｽ�ｽ50�ｽ�ｽ�ｽ�ｽ�ｽﾈ難ｿｽ�ｽﾅ難ｿｽﾍゑｿｽ�ｽﾄゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽB");
+				errmsg.add("メールアドレスは50文字以内で入力してください。");
 			}
 		}
 		/* password Validation */
-		if(StringUtils.isEmpty(password)) {	// �ｽ�ｽﾜゑｿｽ�ｽ�ｽnull
+		if(StringUtils.isEmpty(password)) {	// 空またはnull
 			valid = false;
-			errmsg.add("�ｽp�ｽX�ｽ�ｽ�ｽ[�ｽh�ｽ�ｽ�ｽﾍゑｿｽ�ｽﾄゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽB");
+			errmsg.add("パスワードを入力してください。");
 		} else {
 			if(!password.matches("[0-9a-zA-Z]+")) {
 				valid = false;
-				errmsg.add("�ｽp�ｽX�ｽ�ｽ�ｽ[�ｽh�ｽﾍ費ｿｽ�ｽp�ｽp�ｽ�ｽ�ｽ�ｽ�ｽﾅ難ｿｽﾍゑｿｽ�ｽﾄゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽB");
+				errmsg.add("パスワードは半角英数字で入力してください。");
 			}
 			if(password.length() >= 6 && password.length() <= 8) {
 				valid = false;
-				errmsg.add("�ｽp�ｽX�ｽ�ｽ�ｽ[�ｽh�ｽ�ｽ6~8�ｽ�ｽ�ｽ�ｽ�ｽﾅ難ｿｽﾍゑｿｽ�ｽﾄゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽB");
+				errmsg.add("パスワードは6文字~8文字で入力してください。");
 			}
 		}
 		return valid;
 	}
 	/**
-	 * getBankAddValidation�ｽ�ｽ�ｽ\�ｽb�ｽh : �ｽ�ｽs�ｽ�ｽ�ｽ�ｽo�ｽ^�ｽ�ｽ�ｽ�ｽ�ｽﾉ対ゑｿｽ�ｽ�ｽo�ｽ�ｽ�ｽf�ｽ[�ｽV�ｽ�ｽ�ｽ�ｽ
+	 * getBankAddValidationメソッド : 銀行口座　追加処理
 	 * 
-	 * @param bank_number String: �ｽ�ｽ�ｽ�ｽﾔ搾ｿｽ
-	 * @param branch_code String: �ｽx�ｽX�ｽﾔ搾ｿｽ
-	 * @param bank_persons String: �ｽ�ｽ�ｽ�ｽ�ｽ�ｽ`�ｽl
-	 * @param bank_name String: �ｽ�ｽs�ｽ�ｽ
-	 * @return valid boolean: �ｽo�ｽ�ｽ�ｽf�ｽ[�ｽV�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ
+	 * @param bank_number String: 口座番号
+	 * @param branch_code String: 支店番号
+	 * @param bank_persons String: 口座名義人
+	 * @param bank_name String: 銀行名
+	 * @return valid boolean: バリデーション判定
 	 */
-	public boolean getBankAddValidation(String bank_number, String branch_code,
-			String bank_persons, String bank_name) {
-		// �ｽG�ｽ�ｽ�ｽ[�ｽ�ｽ�ｽb�ｽZ�ｽ[�ｽW�ｽ�ｽ�ｽX�ｽg�ｽ�ｽ�ｽ�ｽ
+	public boolean getBankAddValidation(HttpServletRequest request) {
 		this.errmsg = new ArrayList<String>();
-		// �ｽo�ｽ�ｽ�ｽf�ｽ[�ｽV�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ闖会ｿｽ�ｽ
 		this.valid = true;
+		
+		// 口座番号取得
+		String bank_number = request.getParameter("bank_number");
+		// 支店番号取得
+		String branch_code = request.getParameter("branch_code");
+		// 名義人取得
+		String bank_persons = request.getParameter("bank_persons");
+		// 銀行名取得
+		String bank_name = request.getParameter("bank_name");
 		
 		/* bank_number Validation */
 		if(StringUtils.isEmpty(bank_number)) {
 			valid = false;
-			errmsg.add("�ｽ�ｽ�ｽ�ｽﾔ搾ｿｽ�ｽ�ｽ�ｽﾍゑｿｽ�ｽﾄゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽB");			
+			errmsg.add("口座番号を入力してください。");			
 		} else {
 			if(!bank_number.matches("[0-9]{1,10}")) {
 				valid = false;
-				errmsg.add("�ｽ�ｽ�ｽ�ｽﾔ搾ｿｽ�ｽﾍ費ｿｽ�ｽp�ｽ�ｽ�ｽ�ｽ10�ｽ�ｽ�ｽ�ｽ�ｽﾈ難ｿｽ�ｽﾅ難ｿｽﾍゑｿｽ�ｽﾄ会ｿｽ�ｽ�ｽ�ｽ�ｽ�ｽB");
+				errmsg.add("口座番号は半角数字10文字以内で入力してください。");
 			}
 		}
 		/* branch_code Validation */
 		if(StringUtils.isEmpty(branch_code)) {
 			valid = false;
-			errmsg.add("�ｽx�ｽX�ｽﾔ搾ｿｽ�ｽ�ｽ�ｽﾍゑｿｽ�ｽﾄゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽB");
+			errmsg.add("支店番号を入力してください。");
 		} else {
 			if(!branch_code.matches("[0-9]{3}")) {
 				valid = false;
-				errmsg.add("�ｽx�ｽX�ｽﾔ搾ｿｽ�ｽﾍ費ｿｽ�ｽp�ｽ�ｽ�ｽ�ｽ3�ｽ�ｽ�ｽ�ｽ�ｽﾈ難ｿｽ�ｽﾅ難ｿｽﾍゑｿｽ�ｽﾄ会ｿｽ�ｽ�ｽ�ｽ�ｽ�ｽB");				
+				errmsg.add("支店番号は半角数字3文字以内で入力してください。");				
 			}
 		}
 		/* bank_persons Validation */
 		if(StringUtils.isEmpty(bank_persons)) {
 			valid = false;
-			errmsg.add("�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ`�ｽl�ｽ�ｽ�ｽﾍゑｿｽ�ｽﾄゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽB");			
+			errmsg.add("名義人を入力してください。");			
 		} else {
 			if(bank_persons.length() > 20) {
 				valid = false;
-				errmsg.add("�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ`�ｽl�ｽ�ｽ20�ｽ�ｽ�ｽ�ｽ�ｽﾈ難ｿｽ�ｽﾅ難ｿｽﾍゑｿｽ�ｽﾄゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽB");				
+				errmsg.add("名義人は20文字以内で入力してください。");				
 			}
 		}
 		/* bank_name Validation */
 		if(StringUtils.isEmpty(bank_name)) {
 			valid = false;
-			errmsg.add("�ｽ�ｽs�ｽ�ｽ�ｽ�ｽI�ｽ�ｽ�ｽ�ｽ�ｽﾄゑｿｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽB");				
+			errmsg.add("銀行名を入力してください。");				
 		}
 		
 		return valid;
 	}
 	/**
-	 * getCreditAddValidation�ｽ�ｽ�ｽ\�ｽb�ｽh : �ｽN�ｽ�ｽ�ｽW�ｽb�ｽg�ｽJ�ｽ[�ｽh�ｽo�ｽ^�ｽ�ｽ�ｽ�ｽ�ｽﾉ対ゑｿｽ�ｽ�ｽo�ｽ�ｽ�ｽf�ｽ[�ｽV�ｽ�ｽ�ｽ�ｽ
+	 * getCreditAddValidationメソッド : クレジットカード　追加処理
 	 * 
-	 * @param card_number String : �ｽN�ｽ�ｽ�ｽW�ｽb�ｽg�ｽJ�ｽ[�ｽh�ｽﾔ搾ｿｽ
-	 * @param limit_date String : �ｽL�ｽ�ｽ�ｽ�ｽ
-	 * @param card_company : �ｽN�ｽ�ｽ�ｽW�ｽb�ｽg�ｽJ�ｽ[�ｽh�ｽ�ｽ�ｽ 
-	 * @param sec_code : �ｽZ�ｽL�ｽ�ｽ�ｽ�ｽ�ｽe�ｽB�ｽR�ｽ[�ｽh
-	 * @param card_persons : �ｽJ�ｽ[�ｽh�ｽ�ｽ�ｽ`�ｽl
-	 * @return valid boolean : �ｽo�ｽ�ｽ�ｽf�ｽ[�ｽV�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ
+	 * @param card_number String : クレジットカード番号
+	 * @param limit_date String : 有効期限
+	 * @param card_company : カード会社 
+	 * @param sec_code : セキュリティコード
+	 * @param card_persons : カード名義人
+	 * @return valid boolean : バリデーション判定
 	 */
 	public boolean getCreditAddValidation(String card_number, String limit_date, String card_company,
 			String sec_code, String card_persons) {
-		// �ｽG�ｽ�ｽ�ｽ[�ｽ�ｽ�ｽb�ｽZ�ｽ[�ｽW�ｽ�ｽ�ｽX�ｽg�ｽ�ｽ�ｽ�ｽ
 		this.errmsg = new ArrayList<String>();
-		// �ｽo�ｽ�ｽ�ｽf�ｽ[�ｽV�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ�ｽ闖会ｿｽ�ｽ
 		this.valid = true;
 		
 		/* card_number Validation */
