37f42e249f38833ecc6ef03a279d48592e8eb521
Naventus <u.u.tau216@gmail.com>
2015-02-09 23:39:17 +0900

    FileFactoryクラス追加

---
+++ musia/src/jp/co/musia/FileFactory.java
@@ -0,0 +1,23 @@
+package jp.co.musia;
+
+import java.io.File;
+
+public class FileFactory {
+
+	private String filepath;
+	
+	
+	public String getFilepath() {
+		return this.filepath;
+	}
+	
+	public void setFilePath(String path) {
+		this.filepath = path;
+	}
+	
+	private boolean mkDir(String path) {
+		
+		File f = new File(path);
+		return f.mkdir();
+	}
+}
