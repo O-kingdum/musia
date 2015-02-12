e20e95c7525f3dcb89ae9aeace58e09342da67f5
Naventus <u.u.tau216@gmail.com>
2015-02-12 12:47:52 +0900

    loginfilter追加

---
+++ musia/src/jp/ca/musia/filter/UserLoginFilter.java
@@ -0,0 +1,58 @@
+package jp.ca.musia.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.co.musia.Auth;
+
+/**
+ * Servlet Filter implementation class UserLoginFilter
+ */
+@WebFilter("/UserLoginFilter")
+public class UserLoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public UserLoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		if(!Auth.checkAuth()) {
+			((HttpServletResponse)response).sendRedirect("/");
+		}
+		
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
