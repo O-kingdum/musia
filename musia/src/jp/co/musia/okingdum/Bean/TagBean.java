a1a37b9b0c60f75135ef744368ebdeada40214c8
Naventus <u.u.tau216@gmail.com>
2015-02-17 20:47:10 +0900

    コミット

--- musia/src/jp/co/musia/okingdum/Bean/TagBean.java
+++ musia/src/jp/co/musia/okingdum/Bean/TagBean.java
@@ -5,6 +5,11 @@ public class TagBean {
 	private String tag_id;
 	private String tag_name;
 	
+	public TagBean(String tag_id, String tag_name) {
+		this.tag_id = tag_id;
+		this.tag_name = tag_name;
+	}
+
 	public String getTag_id() {
 		return tag_id;
 	}
