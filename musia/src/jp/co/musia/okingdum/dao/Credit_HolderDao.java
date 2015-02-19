4f3f22b00f9cfe3733b569c3272838880aa6b91e
Yuuki.Takara <Tkray@TkraMac.local>
2015-02-19 14:12:41 +0900

    Credit_HolderBean追加 Credit_HolderDao追加

---
+++ musia/src/jp/co/musia/okingdum/dao/Credit_HolderDao.java
@@ -0,0 +1,163 @@
+package jp.co.musia.okingdum.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import jp.co.musia.okingdum.Bean.Credit_HolderBean;
+
+public class Credit_HolderDao extends Dao{
+	/**
+	 * insertCheckListメソッド : Credit_Holderを追加する
+	 * 
+	 * @param credit_holder
+	 *            Credit_HolderBeanオブジェクト
+	 * @return 成功:1 失敗:-1
+	 */
+	public int insertCredit_Holder(Credit_HolderBean credit_holder) {
+
+		int ret = 0;
+		String sql = "INSERT INTO t_credit_holder VALUES(?,?)";
+
+		try {
+			// コネクション作成
+			this.getConnection();
+			// プリコンパイル
+			ps = con.prepareStatement(sql);
+
+			ps.setString(1, credit_holder.getCredit_id());
+			ps.setString(2, credit_holder.getUser_id());
+
+			// クエリ発行
+			ret = ps.executeUpdate();
+			// エラーフラグ
+			if (ret < 1) {
+				ret = -1;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			ret = -1;
+		} finally {
+			// クローズ
+			this.close();
+		}
+		return ret;
+	}
+	
+	/**
+	 * updateCredit_Holderメソッド
+	 * 
+	 * @param credit_holder Credit_HolderBeanオブジェクト
+	 * @return ret -1:異常終了 0:更新失敗 1:更新成功
+	 */
+	public int updateCredit_Holder(Credit_HolderBean credit_holder) {
+		
+		int ret = 0;
+		String sql = "UPDATE t_credit_holder SET f_user_id=? WHERE f_credit_id=?;";
+		
+		try
+		{
+			// コネクション生成
+			this.getConnection();
+			// プリコンパイル
+			ps = this.con.prepareStatement(sql);
+			// バインドセット
+			ps.setString(1, credit_holder.getUser_id());
+			ps.setString(2, credit_holder.getCredit_id());
+			// クエリ発行
+			ret = ps.executeUpdate();
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+			ret = -1;
+		}
+		finally
+		{
+			// クローズ
+			this.close();
+		}
+		return ret;
+	}
+	
+	/**
+	 * deleteCredit_Holderメソッド
+	 * 
+	 * @param credit_holder Credit_HolderBeanオブジェクト
+	 * @return ret -1:異常終了 0:更新失敗 1:更新成功
+	 */
+	public int deleteCredit_Holder(Credit_HolderBean credit_holder) {
+		
+		int ret = 0;
+		String sql = "DELETE FROM t_credit_holder WHERE f_credit_id=?;";
+		
+		try
+		{
+			// コネクション生成
+			this.getConnection();
+			// プリコンパイル
+			ps = this.con.prepareStatement(sql);
+			// バインドセット
+			ps.setString(1, credit_holder.getCredit_id());
+			// クエリ発行
+			ret = ps.executeUpdate();
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+			ret = -1;
+		}
+		finally
+		{
+			// クローズ
+			this.close();
+		}
+		return ret;
+	}
+	
+	/**
+	 * selectCredit_Holderメソッド
+	 * 
+	 * @param array ArrayList<Object> Credit_HolderBean
+	 * @return retarr ArrayList<Object> 検索結果
+	 */
+	public ArrayList<Object> selectCredit_Holder(ArrayList<Object> array) {
+		
+		String sql = "SELECT t_credit_holder WHERE f_credit_id in('";
+		ArrayList<Object> retarr = new ArrayList<Object>();
+		
+		for(int i = 0; i < array.size(); i++) {
+			sql += ((Credit_HolderBean)array.get(i)).getCredit_id() + "','";
+		}
+		sql += "');";
+		
+		try
+		{
+			// コネクション生成
+			this.getConnection();
+			// ステートメント
+			st = con.createStatement();
+			// クエリ発行
+			rs = st.executeQuery(sql);
+			
+			while(rs.next())
+			{
+				retarr.add(new Credit_HolderBean(
+						rs.getString("f_credit_id"),
+						rs.getString("f_user_id")
+						)
+				);
+			}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			// クローズ
+			this.close();
+		}
+		return retarr;
+	}
+}
