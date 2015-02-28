d8b6fef0ebd4e2dd3471ff310e4d35b2f007b3bf
midoribito <jingaimidoribito@gmail.com>
2015-02-28 10:35:45 +0900

    Reconfiguration of the folder.

--- musia/WebContent/view/cart/select/index.jsp
+++ musia/WebContent/view/cart/select/index.jsp
@@ -1,14 +1,17 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
   pageEncoding="UTF-8"%>
 <jsp:include page="/template/template.jsp">
-  <jsp:param value="ヘルプ | MUSIA" name="siteTitle" />
+  <jsp:param value="楽曲詳細 | カート | MUSIA" name="siteTitle" />
   <jsp:param value="${pageContext.request.contextPath}/css/vendor/reset.css" name="resetCss" />
-  <jsp:param value="${pageContext.request.contextPath}/css/template/template.css" name="templateCss"/>
-  <jsp:param value="${pageContext.request.contextPath}/css/credit.css" name="mainCss" />
+  <jsp:param
+    value="${pageContext.request.contextPath}/css/template/template.css"
+    name="templateCss" />
+  <jsp:param value="${pageContext.request.contextPath}/css/cart_song.css"
+    name="mainCss" />
   <jsp:param name="pageContents">
     <jsp:attribute name="value">
       <article class="article">
-        <h2 class="article-title">ヘルプ</h2>
+        <h2 class="article-title">楽曲投稿</h2>
         <div class="article-content">
           
         </div>
--- musia/src/jp/co/musia/okingdum/MusiaServlet.java
+++ musia/src/jp/co/musia/okingdum/MusiaServlet.java
@@ -11,206 +11,191 @@ import jp.co.musia.okingdum.Utils.*;
 import jp.co.musia.okingdum.dao.*;
 import jp.co.musia.okingdum.Bean.*;
 
-
 /**
  * Servlet implementation class MusiaServlet
  */
 @WebServlet("/MusiaServlet")
 public class MusiaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MusiaServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public MusiaServlet() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+
+    String dispPage = "/view/index.jsp";
+
+    // リスナー登録
+    if ("/listener_signup".equals(request.getRequestURI())) {
+      dispPage = "/view/signup_listener/index.jsp";
+    }
+    // オプション
+    else if ("/option".equals(request.getRequestURI())) {
+      dispPage = "/view/option/index.jsp";
+    }
+    // オプション → 購入履歴
+    else if ("/option/history".equals(request.getRequestURI())) {
+      dispPage = "/view/option/history/index.jsp";
+    }
+    // オプション → リスナー情報
+    else if ("/option/listener".equals(request.getRequestURI())) {
+      dispPage = "/view/option/listener/index.jsp";
+    }
+    // オプション → アーティスト情報
+    else if ("/option/artist".equals(request.getRequestURI())) {
+      dispPage = "/view/option/artist/index.jsp";
+    }
+    // オプション → クレジットカード情報
+    else if ("/option/credit".equals(request.getRequestURI())) {
+      dispPage = "/view/option/credit/index.jsp";
+    }
+    // オプション → ポイント購入
+    else if ("/option/point".equals(request.getRequestURI())) {
+      dispPage = "/view/option/point/index.jsp";
+    }
+    // ユーザログイン
+    else if ("/login_user".equals(request.getRequestURI())) {
+      dispPage = "/view/login_user/index.jsp";
+    }
+    // 気になる
+    else if ("/option/list".equals(request.getRequestURI())) {
+      dispPage = "/view/option/list/index.jsp";
+    }
+    // カート
+    else if ("/cart".equals(request.getRequestURI())) {
+      dispPage = "/view/cart/index.jsp";
+    }
+    // 楽曲詳細
+    else if ("/cart/song".equals(request.getRequestURI())) {
+      dispPage = "/view/cart/song/index.jsp";
+    }
+    // お支払選択
+    else if ("/cart/select".equals(request.getRequestURI())) {
+      dispPage = "/view/cart/select/index.jsp";
+    }
+    // ダウンロード
+    else if ("/cart/download".equals(request.getRequestURI())) {
+      dispPage = "/view/cart/download/index.jsp";
+    }
+    // アーティスト登録
+    else if ("/artist_signup".equals(request.getRequestURI())) {
+      dispPage = "/view/artist_signup/index.jsp";
+    }
+    // リリース
+    else if ("/release".equals(request.getRequestURI())) {
+      dispPage = "/view/release/index.jsp";
+
+    }
+    // リリース登録
+    else if ("/release/song".equals(request.getRequestURI())) {
+      dispPage = "/view/release/song/index.jsp";
+    }
+    // コンテスト
+    else if ("/contest".equals(request.getRequestURI())) {
+      dispPage = "/view/contest/index.jsp";
+    }
+    // コンテスト登録
+    else if ("/contest/song".equals(request.getRequestURI())) {
+      dispPage = "/view/contest/song/index.jsp";
+    }
+    // ヘルプ
+    else if ("/help".equals(request.getRequestURI())) {
+      dispPage = "/view/help/index.jsp";
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-		String dispPage = "/view/index.jsp";
-		
-		if ("/listener_signup".equals(request.getRequestURI()))// ユーザ登録
-		{
-			dispPage = "/view/signup_listener/index.jsp";
-		}
-		else if ("/option".equals(request.getRequestURI()))	// マイページ
-		{
-			dispPage = "/view/option/index.jsp";	
-		}
-		else if ("/option/history".equals(request.getRequestURI()))	// 購入履歴
-		{
-			dispPage = "/view/option/history/index.jsp";
-		}
-		else if ("/option/listener".equals(request.getRequestURI()))	// リスナー情報
-		{
-			dispPage = "/view/option/listener/index.jsp";
-		}
-		else if ("/option/artist".equals(request.getRequestURI()))	// アーティスト情報
-		{
-			dispPage = "/view/option/artist/index.jsp";	
-		}
-		else if ("/option/credit".equals(request.getRequestURI()))	// クレジットカード情報
-		{
-			dispPage = "/view/option/credit/index.jsp";			
-		}
-		else if ("/option/point".equals(request.getRequestURI()))	// ポイント購入
-		{
-			dispPage = "/view/option/point/index.jsp";			
-		}
-		else if ("/login_user".equals(request.getRequestURI()))	// ユーザログイン
-		{
-			dispPage = "/view/login_user/index.jsp";
-		}
-		else if ("/option/list".equals(request.getRequestURI()))		// ほしいものリスト
-		{
-			dispPage = "/view/option/list/index.jsp";
-		}
-		else if ("/cart".equals(request.getRequestURI()))		// カート
-		{
-			dispPage = "/view/cart/index.jsp";
-		}
-		else if ("/cart/select".equals(request.getRequestURI()))		// お支払選択
-		{
-			dispPage = "/view/cart/select/index.jsp";
-		}
-		else if ("/cart/download".equals(request.getRequestURI()))		// ダウンロード
-		{
-			dispPage = "/view/cart/download/index.jsp";	
-		}
-		else if ("/artist_signup".equals(request.getRequestURI()))		// アーティスト登録
-		{
-			dispPage = "/view/artist_signup/index.jsp";
-		}
-		else if ("/release".equals(request.getRequestURI()))		// リリース
-		{
-			dispPage = "/view/release/index.jsp";
-		}
-		else if ("/release/song".equals(request.getRequestURI()))		// リリース登録
-		{
-			dispPage = "/view/release/song/index.jsp";	
-		}
-		else if ("/contest".equals(request.getRequestURI()))		// コンテスト
-		{
-			dispPage = "/view/contest/index.jsp";
-		}
-		else if ("/contest/song".equals(request.getRequestURI()))		// コンテスト登録
-		{
-			dispPage = "/view/contest/song/index.jsp";
-		}
-		else if ("/help".equals(request.getRequestURI()))		// 注文情報変更ページ
-		{
-			dispPage = "/view/help/index.jsp";
-		}
-				
-		request.getRequestDispatcher(dispPage).forward(request, response);
-	}
+    request.getRequestDispatcher(dispPage).forward(request, response);
+  }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-		String dispPage = "";
-		String url = request.getRequestURI();
-		Validator validator = new Validator();
-		
-		
-		switch(url){
-		
-		case "/musia/login_user":					//ユーザログイン
-			
-			if(validator.getLoginValidation(request)){
-				Auth.loginAuth(request);
-				dispPage = "/view/signup_listener/index.jsp";
-			}
-			else{
-				response.sendRedirect(request.getContextPath());
-				return;
-			}
-			
-			
-			break;
-			
-		case "/musia/option/credit":				//クレジット追加
-			
-			if(validator.getCreditAddValidation(request)){
-				CreditCardDao credao = new CreditCardDao();
-				credao.insertCreditCard(
-						new CreditCardBean(
-								request.getParameter("credit_id"),
-								Integer.parseInt(request.getParameter("card_number")),
-								request.getParameter("limit_date"),
-								request.getParameter("card_company"),
-								Integer.parseInt("sec_cord"),
-								request.getParameter("card_persons")
-								)
-						);
-				
-				dispPage = "/view/option/credit/index.jsp";
-			}
-			else{
-				response.sendRedirect(request.getContextPath());
-				return;
-			}
-			
-			break;
-			
-		case "/musia/listener_signup":				//ユーザ登録
-			
-			if(validator.getCreateUserValidation(request)){
-				UserDao udao = new UserDao();
-				UsersBean user = new UsersBean();
-				
-				user.setEmail( request.getParameter("email") );
-				user.setUser_name( request.getParameter("name") );
-				user.setPassword( request.getParameter("password") );
-				user.setSex( Integer.parseInt( 
-						request.getParameter("sex") ) );
-				user.setBirthday( request.getParameter("birthday") );
-				
-				udao.insertUser( user);
-				
-			}
-			else{
-				response.sendRedirect(request.getContextPath());
-				return;
-			}
-			
-			
-			break;
-			
-		case "/musia/artist_signup":				//アーティスト登録(銀行口座登録)
-			
-			if(validator.getBankAddValidation(request)){
-				UserDao udao = new UserDao();
-				UsersBean user = new UsersBean();
-				user.setBank_number(Integer.parseInt( 
-						request.getParameter("bank_number")));
-				user.setBank_persons("bank_persons");
-				user.setBranch_code(Integer.parseInt( 
-						request.getParameter("bank_code")));
-				user.setBank_name("bank_name");
-				
-				udao.insertUser(user);		
-				
-			}
-			else{
-				response.sendRedirect(request.getContextPath());
-				return;
-			}
-			
-			
-			
-			break;
-		
-		}
-	}
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+
+    String dispPage = "";
+    String url = request.getRequestURI();
+    Validator validator = new Validator();
+
+    switch (url) {
+
+    case "/musia/login_user": // ユーザログイン
+
+      if (validator.getLoginValidation(request)) {
+        Auth.loginAuth(request);
+        dispPage = "/view/signup_listener/index.jsp";
+      } else {
+        response.sendRedirect(request.getContextPath());
+        return;
+      }
+
+      break;
+
+    case "/musia/option/credit": // クレジット追加
+
+      if (validator.getCreditAddValidation(request)) {
+        CreditCardDao credao = new CreditCardDao();
+        credao.insertCreditCard(new CreditCardBean(request.getParameter("credit_id"), Integer.parseInt(request.getParameter("card_number")), request.getParameter("limit_date"), request.getParameter("card_company"), Integer.parseInt("sec_cord"), request.getParameter("card_persons")));
+
+        dispPage = "/view/option/credit/index.jsp";
+      } else {
+        response.sendRedirect(request.getContextPath());
+        return;
+      }
+
+      break;
+
+    case "/musia/listener_signup": // ユーザ登録
+
+      if (validator.getCreateUserValidation(request)) {
+        UserDao udao = new UserDao();
+        UsersBean user = new UsersBean();
+
+        user.setEmail(request.getParameter("email"));
+        user.setUser_name(request.getParameter("name"));
+        user.setPassword(request.getParameter("password"));
+        user.setSex(Integer.parseInt(request.getParameter("sex")));
+        user.setBirthday(request.getParameter("birthday"));
+
+        udao.insertUser(user);
+
+      } else {
+        response.sendRedirect(request.getContextPath());
+        return;
+      }
+
+      break;
+
+    case "/musia/artist_signup": // アーティスト登録(銀行口座登録)
+
+      if (validator.getBankAddValidation(request)) {
+        UserDao udao = new UserDao();
+        UsersBean user = new UsersBean();
+        user.setBank_number(Integer.parseInt(request.getParameter("bank_number")));
+        user.setBank_persons("bank_persons");
+        user.setBranch_code(Integer.parseInt(request.getParameter("bank_code")));
+        user.setBank_name("bank_name");
+
+        udao.insertUser(user);
+
+      } else {
+        response.sendRedirect(request.getContextPath());
+        return;
+      }
+
+      break;
+
+    }
+  }
 
 }
